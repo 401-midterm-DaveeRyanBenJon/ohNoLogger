3058650d8a0af9a57ee7838e90f6210846dad2e7
BEN <benjamin.hill28@gmail.com>
2020-10-24 21:04:13 -0700

    added hub.py, started to build error logger class

---
+++ hub.py
@@ -0,0 +1,24 @@
+# # CLASS
+# # __init__ = constructor
+# # self = javascript equivalent of "this" but can be named anything
+# class Error_Hub:
+#     def __init__(self(could be called 'ErrorHubInstance'), first, last):
+#         self.first = first
+#         self.last = last
+
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
+
+
+# # INSTANCE OF CLASS
+# emp_1 = Error_Hub('Ben', 'Hill')
+# emp_2 = Error_Hub('Davee', 'Sok')
+
+# print(emp_1.fullname())
+# print(emp_2)
+
+class ErrorHub:
+    def log_error(self, userid=null, userparam=null, usernote=null):
+        errors: e
+        userid: userid
+        userpararm: userparam
